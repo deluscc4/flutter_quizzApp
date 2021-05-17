eb1bee6b756505db23b2452d5a090c9571e5c198
lucasfv1 <lucasfagundesvaz@gmail.com>
2021-05-17 09:42:29 -0300

    Initial Commit

---
+++ android/app/src/main/java/quizapp/estruturadesafio/polimorfismo/quizapp_estrutura_desafio/MainActivity.java
@@ -0,0 +1,6 @@
+package quizapp.estruturadesafio.polimorfismo.quizapp_estrutura_desafio;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
